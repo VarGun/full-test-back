e85d40e1f035c1fabc3e637452f26d40f554ea08
VarGun <heegun46900@gmail.com>
2025-08-04 15:27:06 +0900

    flow 추가

--- src/main/java/com/example/demo3/config/WebConfig.java
+++ src/main/java/com/example/demo3/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/**")
-        .allowedOrigins("http://localhost:5173")
+        .allowedOrigins("http://fullstack-frontend-app-bucket-heegun.s3-website.ap-northeast-2.amazonaws.com/")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
